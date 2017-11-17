16e237d20111c92c2a514f1d64cc1707dc356d05
Nicholas Liu <hkai.liu@gmail.com>
2017-11-17 18:09:30 -0500

    fix: css delete, use Semantics UI instead

--- app/containers/index.jsx
+++ app/containers/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
+
 import Welcome from './Welcome'
 import Lobby from './Lobby'
 import Room from './Room'
