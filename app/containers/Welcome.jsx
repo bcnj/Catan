66c782f12a528e78beca35c1a8993fa8c7c54732
Nicholas Liu <hkai.liu@gmail.com>
2017-11-17 16:18:56 -0500

    fix: front end folder structure setup

--- app/containers/Welcome.jsx
+++ app/containers/Welcome.jsx
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom'
 
 export default () => (
 
-    <div>
+  <div>
     <div className='landing'>
       <div className='mx-auto'>
         <div className='display-4 pb-3'> Settlers of Catan </div>
         <div>
-        <div style={{textAlign: 'center'}}>
-          <Link to='/lobby'>
-          <Button color='green'> Join Game </Button></Link>
-          <Link to='/lobby'><Button color='red'> Create Game </Button></Link>
-        </div>
+          <div style={{ textAlign: 'center' }}>
+            <Link to='/lobby'>
+              <Button color='green'> Join Game </Button></Link>
+            <Link to='/lobby'><Button color='red'> Create Game </Button></Link>
+          </div>
         </div>
       </div>
     </div>
